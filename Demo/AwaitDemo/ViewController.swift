720efc4d60096db7946c3420a8726d85a0a79fa8
Yasuhiro Inami <inamiy@gmail.com>
2014-07-04 11:35:36 +0900

    [Demo] Update for UI testing.

--- Demo/AwaitDemo/ViewController.swift
+++ Demo/AwaitDemo/ViewController.swift
@@ -13,35 +13,69 @@ class ViewController: UITableViewController {
     
     let request = NSURLRequest(URL: NSURL(string:"https://github.com"))
     
+    struct DebugOptions
+    {
+        // To test how RunLoop works with/without queueing.
+        // NOTE: Many UI e.g. button-highlighting won't update when set to `false`.
+        static let shouldStartAwaitAfterDelay = false
+        
+        //
+        // NOTE:
+        // Setting `true` requires `shouldStartAwaitAfterDelay=true` as well for better UI response, 
+        // since viewDidAppear is called via _dispatch_main_queue_callback_4CF.
+        //
+        static let shouldStartAwaitOnViewDidAppear = false
+    }
+    
     override func viewDidLoad()
     {
+        NSLog("viewDidLoad")
         super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(animated: Bool)
+    {
+        NSLog("viewWillAppear")
+        super.viewWillAppear(animated)
     }
     
     override func viewDidAppear(animated: Bool)
     {
+        NSLog("viewDidAppear")
         super.viewDidAppear(animated)
         
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 1_000_000_000), dispatch_get_main_queue()) {
-            self._startTest()
+        if DebugOptions.shouldStartAwaitOnViewDidAppear {
+            self.startAwait()
         }
     }
     
-    func _startTest()
+    @IBAction func startAwait()
+    {
+        if DebugOptions.shouldStartAwaitAfterDelay {
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 1_000_000_000), dispatch_get_main_queue()) {
+                self._startAwait()
+            }
+        }
+        else {
+            self._startAwait()
+        }
+    }
+    
+    func _startAwait()
     {
         SVProgressHUD.showWithStatus("loading...")
         
         NSLog("await start")
         
-//        let data = self._performAwait()
+        let data = self._performAwait()
 //        let data = self._performAwaitWithTimeout()
-        let data = self._performAwaitWithPromiseKit()
+//        let data = self._performAwaitWithPromiseKit()
         
         NSLog("await end")
         
         if let data_ = data {
             let html = NSString(data: data, encoding: NSUTF8StringEncoding)
-            println(html)
+//            println(html)
             
             SVProgressHUD.showSuccessWithStatus("Success!")
         }
@@ -55,7 +89,8 @@ class ViewController: UITableViewController {
     
     func _performAwait() -> NSData?
     {
-        return await { NSURLConnection.sendSynchronousRequest(self.request, returningResponse:nil, error: nil) }
+        return await { sleep(3); return NSData() /* dummy */ }
+//        return await { NSURLConnection.sendSynchronousRequest(self.request, returningResponse:nil, error: nil) }
     }
     
     func _performAwaitWithTimeout() -> NSData?
@@ -64,7 +99,7 @@ class ViewController: UITableViewController {
         
         return await({
             sleep(CUnsignedInt(timeout)+1)  // sleep well to demonstrate timeout error
-            return NSData()
+            return NSData() // dummy
         }, timeout: timeout)
     }
     
