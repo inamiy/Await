12fc7bc6225855fe4042cfb83ffe447d0874a95f
Yasuhiro Inami <inamiy@gmail.com>
2014-07-05 08:57:56 +0900

    [Demo] Clean up code & use async to wrap await calls.

--- Demo/AwaitDemo/ViewController.swift
+++ Demo/AwaitDemo/ViewController.swift
@@ -9,22 +9,11 @@
 import UIKit
 import PromiseKit
 
-class ViewController: UITableViewController {
-    
-    let request = NSURLRequest(URL: NSURL(string:"https://github.com"))
-    
+class ViewController: UITableViewController
+{
     struct DebugOptions
     {
-        // To test how RunLoop works with/without queueing.
-        // NOTE: Many UI e.g. button-highlighting won't update when set to `false`.
-        static let shouldStartAwaitAfterDelay = false
-        
-        //
-        // NOTE:
-        // Setting `true` requires `shouldStartAwaitAfterDelay=true` as well for better UI response, 
-        // since viewDidAppear is called via _dispatch_main_queue_callback_4CF.
-        //
-        static let shouldStartAwaitOnViewDidAppear = false
+        static let shouldStartAwaitOnViewDidAppear = true
     }
     
     override func viewDidLoad()
@@ -51,70 +40,62 @@ class ViewController: UITableViewController {
     
     @IBAction func startAwait()
     {
-        if DebugOptions.shouldStartAwaitAfterDelay {
-            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 1_000_000_000), dispatch_get_main_queue()) {
-                self._startAwait()
+        //
+        // NOTE:
+        // For applicaiton use, you MUST wrap `await` calls with `async`.
+        // This is to ensure that `await` will not block `dispatch_get_main_queue()`
+        // which often causes UIKit not responding to touches.
+        //
+        // LIMITATION:
+        // `await(_:timeout:)` may not work properly inside `async` due to nested RunLoop running.
+        //
+        async {
+            SVProgressHUD.showWithStatus("Loading...")
+            
+            NSLog("await start")
+            
+            let result = self._performAwait()
+//            let result = self._performAwaitWithTimeout()
+//            let result = self._performAwaitWithPromiseKit()
+            
+            NSLog("await end")
+            
+            if let result_ = result {
+//                NSLog("result = \(result_)")
+                SVProgressHUD.showSuccessWithStatus("Success!")
+            }
+            else {
+                let message = "Failed: either execution failed or timed out"
+                NSLog(message)
+                
+                SVProgressHUD.showErrorWithStatus(message)
             }
         }
-        else {
-            self._startAwait()
-        }
     }
     
-    func _startAwait()
+    func _performAwait() -> Any?
     {
-        SVProgressHUD.showWithStatus("loading...")
-        
-        NSLog("await start")
-        
-        let data = self._performAwait()
-//        let data = self._performAwaitWithTimeout()
-//        let data = self._performAwaitWithPromiseKit()
-        
-        NSLog("await end")
-        
-        if let data_ = data {
-            let html = NSString(data: data, encoding: NSUTF8StringEncoding)
-//            println(html)
-            
-            SVProgressHUD.showSuccessWithStatus("Success!")
-        }
-        else {
-            let message = "Failed: either execution failed or timed out"
-            println(message)
-            
-            SVProgressHUD.showErrorWithStatus(message)
-        }
+        return await { sleep(3) }
     }
     
-    func _performAwait() -> NSData?
+    func _performAwaitWithTimeout() -> Any?
     {
-        return await { sleep(3); return NSData() /* dummy */ }
-//        return await { NSURLConnection.sendSynchronousRequest(self.request, returningResponse:nil, error: nil) }
+        return await({ sleep(2) }, timeout: 1)
     }
     
-    func _performAwaitWithTimeout() -> NSData?
-    {
-        let timeout = 1.0
-        
-        return await({
-            sleep(CUnsignedInt(timeout)+1)  // sleep well to demonstrate timeout error
-            return NSData() // dummy
-        }, timeout: timeout)
-    }
-    
-    func _performAwaitWithPromiseKit() -> NSData?
+    func _performAwaitWithPromiseKit() -> Any?
     {
         let queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
 //        let queue = dispatch_get_main_queue()
         
-        let promise = Promise<NSData> { [weak self] (fulfiller, rejecter) in
+        let promise = Promise<NSData> { (fulfiller, rejecter) in
             dispatch_async(queue) {
-                let data = NSURLConnection.sendSynchronousRequest(self!.request, returningResponse:nil, error: nil)
+                let request = NSURLRequest(URL: NSURL(string:"https://github.com"))
+                let data = NSURLConnection.sendSynchronousRequest(request, returningResponse:nil, error: nil)
                 fulfiller(data)
             }
-        }.then(onQueue: queue) { (data: NSData) -> NSData in
-            return data
+        }.then(onQueue: queue) { (data: NSData) -> NSString in
+            return NSString(data: data, encoding: NSUTF8StringEncoding)
         }
         
 //        return await({ promise.value }, until: { !promise.pending } )
